543e5542891664e8906e75904f19d6a7aaac5abb
samuelborn@outlook.de <samuelborn@outlook.de>
2021-12-30 11:54:35 +0100

    default selection for input

--- UserInterface/ConstraintsInputFrame.py
+++ UserInterface/ConstraintsInputFrame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 from Utils import Utils2D
 from UserInterface import InputConverter
@@ -12,19 +13,20 @@ class ConstraintsInputFrame(tk.Frame):
         tk.Frame.__init__(self, root)
         self.root = root
 
-        self.introduction = tk.Label(self, text="Eingabe der Zielfunktion und NB", font='Helvetica 18 bold')#, font=controller.title_font)
+        self.introduction = tk.Label(self, text="Eingabe der Zielfunktion und NB",
+                                     font='Helvetica 18 bold')  # , font=controller.title_font)
         self.introduction.grid(row=0, column=0, pady=10, padx=10, columnspan=1000)
 
         # place the target function line
         self.min_max = tk.StringVar()
-        self.min_max_option_menu = tk.OptionMenu(self, self.min_max, *["min", "max"])
+        self.min_max_option_menu = ttk.OptionMenu(self, self.min_max, "max", *["min", "max"])
         self.min_max_option_menu.grid(row=1, column=0)
         self.target_function_entries = []
         for i in range(1, num_vars + 1):
             target_function_entry = tk.Entry(self, width=4)
             target_function_entry.grid(row=1, column=2 * i - 1)
             self.target_function_entries.append(target_function_entry)
-            text = f"x{i-1}".translate(SUB) if i == num_vars else f"x{i-1} + ".translate(SUB)
+            text = f"x{i - 1}".translate(SUB) if i == num_vars else f"x{i - 1} + ".translate(SUB)
             target_function_label = tk.Label(self, text=text)
             target_function_label.grid(row=1, column=2 * i)
 
@@ -42,7 +44,7 @@ class ConstraintsInputFrame(tk.Frame):
                 constraint_label.grid(row=row + 2, column=2 * column + 2)
 
             comparative_operator = tk.StringVar()
-            comparative_operator_option_menu = tk.OptionMenu(self, comparative_operator, *["<=", ">=", "="])
+            comparative_operator_option_menu = ttk.OptionMenu(self, comparative_operator, "<=", *["<=", ">=", "="])
             comparative_operator_option_menu.grid(row=row + 2, column=2 * num_vars + 1)
             self.comparative_operators.append(comparative_operator)
 
--- UserInterface/KeyDataFrame.py
+++ UserInterface/KeyDataFrame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 from UserInterface.ConstraintsInputFrame import ConstraintsInputFrame
 from UserInterface.ResultsFrame import ResultsFrame
@@ -9,19 +10,20 @@ class KeyDataFrame(tk.Frame):
         tk.Frame.__init__(self, root)
         self.root = root
 
-        self.introduction = tk.Label(self, text="Eingabe der Eckdaten", font='Helvetica 18 bold')  # , font=controller.title_font)
+        self.introduction = tk.Label(self, text="Eingabe der Eckdaten",
+                                     font='Helvetica 18 bold')  # , font=controller.title_font)
         self.introduction.grid(row=0, column=0, pady=10, padx=10)
 
         self.var_count_label = tk.Label(self, text="Anzahl der Variablen:")
         self.var_count_label.grid(row=1, column=0, pady=10, padx=10)
         self.var_count_entry = tk.Entry(self)
-        self.var_count_entry.grid(row=1, column=1)
+        self.var_count_entry.grid(row=1, column=1, pady=10, padx=10)
         self.var_count_entry.insert(tk.END, "2")
 
         self.constraints_count_label = tk.Label(self, text="Anzahl der Nebenbedinungen.")
         self.constraints_count_label.grid(row=2, column=0, pady=10, padx=10)
         self.constraints_count_entry = tk.Entry(self)
-        self.constraints_count_entry.grid(row=2, column=1)
+        self.constraints_count_entry.grid(row=2, column=1, pady=10, padx=10)
         self.constraints_count_entry.insert(tk.END, "1")
 
         self.show_integer_points = tk.BooleanVar()
@@ -29,10 +31,17 @@ class KeyDataFrame(tk.Frame):
         self.show_integer_points_label = tk.Label(self, text="Sollen ganzzahlige Punkte angezigt werden? ")
         self.show_integer_points_label.grid(row=3, column=0, pady=10, padx=10)
         self.show_integer_points_check_box = tk.Checkbutton(self, var=self.show_integer_points)
-        self.show_integer_points_check_box.grid(row=3, column=1)
+        self.show_integer_points_check_box.grid(row=3, column=1, pady=10, padx=10)
+
+        self.selection_rule_label = tk.Label(self, text="Welche Auswahlregel soll genutzt werden?")
+        self.selection_rule_label.grid(row=4, column=0, pady=10, padx=10)
+        self.selection_rule = tk.StringVar()
+        options = ["Last In - First Out", "Maximum Upper Bound"]
+        self.selection_rule_menu = ttk.OptionMenu(self, self.selection_rule, options[0], *options)
+        self.selection_rule_menu.grid(row=4, column=1, pady=10, padx=10)
 
         self.button = tk.Button(self, text="Nächster Schritt", command=self.next_step, bg="#659666")
-        self.button.grid(row=4, column=0, pady=10, padx=10)
+        self.button.grid(row=5, column=0, pady=10, padx=10)
 
     def next_step(self):
         try:
@@ -48,5 +57,3 @@ class KeyDataFrame(tk.Frame):
         y = ConstraintsInputFrame(self.root, var_count, constraint_count, self.show_integer_points.get())
         y.grid(row=0, column=0, sticky="nsew")
         y.tkraise()
-
-
