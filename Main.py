20c4f6ce39b6af8013d16d2f5fa432d6a77c89f3
samuelborn@outlook.de <samuelborn@outlook.de>
2022-01-04 15:55:47 +0100

    refactor

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ from LinearProgram import LinearProgram
 from UserInterface.KeyDataFrame import KeyDataFrame
 
 if __name__ == '__main__':
-
     root = tk.Tk()
     frame = KeyDataFrame(root)
     frame.grid(row=0, column=0, sticky="news")
